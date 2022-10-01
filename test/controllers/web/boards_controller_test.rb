1ca0a74a354e9793118d49ef03d6d4796f7b9f1d
effgenij <effgenij@gmail.com>
2022-10-02 00:43:23 +0300

    Added authenticate to boards

--- test/controllers/web/boards_controller_test.rb
+++ test/controllers/web/boards_controller_test.rb
@@ -1,7 +1,12 @@
-require "test_helper"
+require 'test_helper'
 
 class Web::BoardsControllerTest < ActionController::TestCase
-  test "should get show" do
+  setup do
+    user = create(:user)
+    sign_in user
+  end
+
+  test 'should get show' do
     get :show
     assert_response :success
   end
