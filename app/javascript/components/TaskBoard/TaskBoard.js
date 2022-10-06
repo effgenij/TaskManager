9eaa29f698c3b95930d5277a792fee2e5516d62f
effgenij <effgenij@gmail.com>
2022-10-06 16:22:38 +0300

    Added ColumnHeader and loadColumnMore to TaskBoard

--- app/javascript/components/TaskBoard/TaskBoard.js
+++ app/javascript/components/TaskBoard/TaskBoard.js
@@ -1,27 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import KanbanBoard from '@asseinfo/react-kanban';
-import { propOr } from 'ramda';
+import React, { useEffect, useState } from "react";
+import KanbanBoard from "@asseinfo/react-kanban";
+import { propOr } from "ramda";
 
-import Task from 'components/Task';
-import TasksRepository from 'repositories/TasksRepository';
+import Task from "components/Task";
+import TasksRepository from "repositories/TasksRepository";
+import ColumnHeader from "components/ColumnHeader";
 
 const STATES = [
-  { key: 'new_task', value: 'New' },
-  { key: 'in_development', value: 'In Dev' },
-  { key: 'in_qa', value: 'In QA' },
-  { key: 'in_code_review', value: 'in CR' },
-  { key: 'ready_for_release', value: 'Ready for release' },
-  { key: 'released', value: 'Released' },
-  { key: 'archived', value: 'Archived' },
+  { key: "new_task", value: "New" },
+  { key: "in_development", value: "In Dev" },
+  { key: "in_qa", value: "In QA" },
+  { key: "in_code_review", value: "in CR" },
+  { key: "ready_for_release", value: "Ready for release" },
+  { key: "released", value: "Released" },
+  { key: "archived", value: "Archived" },
 ];
 
 const initialBoard = {
-  columns: STATES.map(column => ({
+  columns: STATES.map((column) => ({
     id: column.key,
     title: column.value,
     cards: [],
     meta: {},
-  }))
+  })),
 };
 
 const TaskBoard = () => {
@@ -49,26 +50,49 @@ const TaskBoard = () => {
     });
   };
 
+  const loadColumnMore = (state, page = 1, perPage = 10) => {
+    loadColumn(state, page, perPage).then(({ data }) => {
+      setBoardCards((prevState) => {
+        return {
+          ...prevState,
+          [state]: {
+            cards: prevState[state].cards.concat(data.items),
+            meta: data.meta,
+          },
+        };
+      });
+    });
+  };
+
   const generateBoard = () => {
     const board = {
       columns: STATES.map(({ key, value }) => {
         return {
           id: key,
           title: value,
-          cards: propOr({}, 'cards', boardCards[key]),
-          meta: propOr({}, 'meta', boardCards[key]),
-        }
-      })
-    }
+          cards: propOr({}, "cards", boardCards[key]),
+          meta: propOr({}, "meta", boardCards[key]),
+        };
+      }),
+    };
 
     setBoard(board);
-  }
+  };
 
   const loadBoard = () => {
     STATES.map(({ key }) => loadColumnInitial(key));
   };
 
-  return <KanbanBoard renderCard={card => <Task task={card} />}>{board}</KanbanBoard>;
+  return (
+    <KanbanBoard
+      renderColumnHeader={(column) => (
+        <ColumnHeader column={column} onLoadMore={loadColumnMore} />
+      )}
+      renderCard={(card) => <Task task={card} />}
+    >
+      {board}
+    </KanbanBoard>
+  );
 };
 
 export default TaskBoard;
